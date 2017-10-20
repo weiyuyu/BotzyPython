08df17ab63de77770af281bdce429997b0cfdb5c
weiyuyu <weiyuyu@usc.edu>
2017-10-20 14:46:22 -0700

    Update

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-                    if message_text.lower() == "what's your name":
+                    if "what's your name" in message_text:
                         send_message(sender_id, "Hi! My name is Botzy!")
                     else:
                         send_message(sender_id, "Roger that!")
