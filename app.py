a608bcfac60e7ce1224f10dd3cbc3cfd5cc54cfa
weiyuyu <weiyuyu@usc.edu>
2017-10-21 01:24:26 -0700

    Added name recognition

--- app.py
+++ app.py
@@ -43,24 +43,24 @@ def webhook():
                 if messaging_event.get("message"):  # someone sent us a message
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    response = requests.get("https://graph.facebook.com/%s?fields=first_name&access_token=EAAa2iYe9rN0BAHB6RcQrGikXL7QfZCAwEQv3ZBCl7BFg5yibbShQTfxhraSNaE4UqZCLcbV0uKP9ADnCHA83CtvVum2K0vZB0ShmWGvdpPX4OH7YyWNT1w9gxrNdomxiEjZClfbZBcZBZAQRjCUhwNfsOyWt2VBH18zIWDALFEOWvjZA7NSKga1yw"%(sender_id))
-                    user_data = response.json()
+                    fb_api_response = requests.get("https://graph.facebook.com/%s?fields=first_name&access_token=EAAa2iYe9rN0BAHB6RcQrGikXL7QfZCAwEQv3ZBCl7BFg5yibbShQTfxhraSNaE4UqZCLcbV0uKP9ADnCHA83CtvVum2K0vZB0ShmWGvdpPX4OH7YyWNT1w9gxrNdomxiEjZClfbZBcZBZAQRjCUhwNfsOyWt2VBH18zIWDALFEOWvjZA7NSKga1yw"%(sender_id))
+                    user_data = fb_api_response.json()
                     sender_first_name = user_data["first_name"]
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-                    #if "hello" in message_text.lower() or "hi" in message_text.lower():
-                        #send_message(sender_id, "Hi %s, my name is Botzy!"%(sender_first_name))
-                    #elif "youtube" in message_text.lower():
-                        #search_title = message_text[8:]
-                        #vid_ids, vid_titles = youtube_search(search_title)
+                    if "hello" in message_text.lower() or "hi" in message_text.lower():
+                        send_message(sender_id, "Hi %s, my name is Botzy!"%(sender_first_name))
+                    elif "youtube" in message_text.lower():
+                        search_title = message_text[8:]
+                        vid_ids, vid_titles = youtube_search(search_title)
 
-                        #send_message(sender_id, "Here are some results for your search")
-                        #for i, vid in enumerate(vid_titles):
-                            #send_message(sender_id, "%s: https://www.youtube.com/watch?v=%s"%(vid,vid_ids[i]))
+                        send_message(sender_id, "Here are some results for your search!")
+                        for i, vid in enumerate(vid_titles):
+                            send_message(sender_id, "%s: https://www.youtube.com/watch?v=%s"%(vid,vid_ids[i]))
 
-                    #else:
-                    send_message(sender_id, "Sorry, %s. I don't understand that yet T.T"%(sender_first_name))
+                    else:
+                        send_message(sender_id, "Sorry, %s. I don't understand that yet T.T"%(sender_first_name))
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
