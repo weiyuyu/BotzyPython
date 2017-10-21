3328097637c84b0d5c804095667fd6d5234147ec
weiyuyu <weiyuyu@usc.edu>
2017-10-21 00:52:01 -0700

    Added name recognition

--- app.py
+++ app.py
@@ -44,13 +44,13 @@ def webhook():
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     json_string = requests.get("https://graph.facebook.com/%s?fields=first_name&access_token=EAAa2iYe9rN0BAHB6RcQrGikXL7QfZCAwEQv3ZBCl7BFg5yibbShQTfxhraSNaE4UqZCLcbV0uKP9ADnCHA83CtvVum2K0vZB0ShmWGvdpPX4OH7YyWNT1w9gxrNdomxiEjZClfbZBcZBZAQRjCUhwNfsOyWt2VBH18zIWDALFEOWvjZA7NSKga1yw"%(sender_id))
-                    print str(json_string)
-                    #sender_first_name = json.load(json_string)[0]["first_name"]
+                    
+                    sender_first_name = json.load(json_string)["first_name"]
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
                     if "hello" in message_text.lower() or "hi" in message_text.lower():
-                        send_message(sender_id, "Hi %s, my name is Botzy!"%(sender_id))
+                        send_message(sender_id, "Hi %s, my name is Botzy!"%(sender_first_name))
                     elif "youtube" in message_text.lower():
                         search_title = message_text[8:]
                         vid_ids, vid_titles = youtube_search(search_title)
